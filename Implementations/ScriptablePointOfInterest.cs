8ecdf6c4eecdab5ee0a2057cee3a6360107f7263
FedoraGameDev <fedora.game.dev@gmail.com>
2021-06-05 12:48:27 -0700

    Initial Commit

---
+++ Implementations/ScriptablePointOfInterest.cs
@@ -0,0 +1,10 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FedoraDev.PointOfInterest.Implementations
+{
+    public class ScriptablePointOfInterest : SerializedScriptableObject
+    {
+        //
+    }
+}
---
+++ Public/IPointOfInterest.cs
@@ -0,0 +1,7 @@
+namespace FedoraDev.PointOfInterest
+{
+    public interface IPointOfInterest
+    {
+        //
+    }
+}
